09b9f3572848d90eb5f478c2af8d32cb1326f167
manishbhaiii <161291689+manishbhaiii@users.noreply.github.com>
2024-07-20 09:49:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "MTA4NTczNjM2MTA1MjYyMjk3OQ.GD7B8e.C68tGIN3Sdsu0CSx7_7t_Cj8Uzx5yiB4ABQHtk",
+    token: "MTA4NTczNjM2MTA1MjYyMjk3OQ.GNZugh.57wNqGLKTS7bQ3tG5-DzvAdPeKknnc1L3fxnF4",
     prefix: ".",
     allowedUserIDs: ['1152622202957598891', '1185297970548178944', '1123856956780728411']
 };
